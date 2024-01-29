472605fe6221c3c3ba43a372bc070bd01c67f0b5
Joshua Caleb Bolito <bolitojoshuacaleb@gmail.com>
2024-01-29 10:44:06 +0800

    [FIX] Changed shadcn/ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <Image src="/logo.png" alt="" width={706} height={501} className="w-12" />
       <ModeToggle />
     </main>
   );
